245842b4c49bb26d1046ce4d7cb71efce3cfd8b5
Kammi Henderson <kammi_henry@yahoo.com>
2024-10-23 14:20:16 -0500

    Filter quotes by category

--- src/components/quotes/QuoteCard.js
+++ src/components/quotes/QuoteCard.js
@@ -4,13 +4,20 @@ const QuoteCard = ({ quote }) => {
     return (
         <article className="quote-card">
         <div>
+            <p className='categories'>
+            {quote.categories.map((category) => (
+                <span className='category' key={category}>
+                    {category}
+                    </span>
+            ))}
+            </p>
             <h3>{quote.text}</h3>
         </div>
         <footer>
             <p className="author">{quote.author}</p>
         </footer>
        </article>
-    )
+    );
 }
 
 export default QuoteCard;
--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -1,11 +1,18 @@
 import React from "react";
 import QuoteCard from "./QuoteCard";
+import CategoryForm from "./CategoryForm";
 
-const Quotes = ({quotes}) => {
+const Quotes = ({ filteredQuotes, categories, category, handleCategoryChange }) => {
     return (
     <section className='all-quotes'>
+        <div className="category-header">
         <div className='quotes wrapper'>
-            {quotes.map(quote => (
+            <p>Browse through your collection of quotes.</p>
+            <CategoryForm categories={categories} category={category} handleCategoryChange={handleCategoryChange} />
+            </div>
+
+        
+            {filteredQuotes.map((quote) => (
                 <QuoteCard key={quote.id} quote={quote} />
             ))}
         </div>
