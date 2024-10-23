e0d1566e9922b808503af3da1201dec4f9701b4b
Kammi Henderson <kammi_henry@yahoo.com>
2024-10-23 16:11:54 -0500

    Favorite quotes part 1

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
   const [quotes, setQuotes] = useState([]);
   const [loading, setLoading] = useState(false);
   const [category, setCategory] = useState("All");
+  const [favoriteQuotes, setFavoriteQuotes] = useState([]);
+  const maxFaves = 3;
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
     const categories = ["All", "Leadership", "Empathy", "Motivation", "Learning", "Success", "Empowerment"];
@@ -31,14 +33,36 @@ function App() {
 
     const filteredQuotes = category !=="All" ? quotes.filter((quote) => quote.categories.includes(category)) : quotes;
 
+
     const handleCategoryChange = (e) => {
       setCategory(e.target.value)
+    };
+
+    const addToFavorites = (quoteId) => {
+      const selectedQuote = quotes.find(quote => quote.id === quoteId);
+
+      const alreadyFavorite = favoriteQuotes.find((favorite) => favorite.id === selectedQuote.id)
+
+      if (alreadyFavorite) {
+        console.log("You already favorited this quote!");
+      } else if (favoriteQuotes.length < maxFaves){
+        setFavoriteQuotes([...favoriteQuotes, selectedQuote])
+        console.log("Added to Favorites!");
+      } else {
+        console.log("Max number of quotes reached. Please delete on to add a new one.")
+      }
     };
 
   return (
     <div className='App'>
       <Header />
       <main>
+        <section className='favorite-quotes'>
+          <div className='wrapper quotes'>
+            <h3>Top 3 favorite quotes</h3>
+            {favoriteQuotes.length > 0 && JSON.stringify(favoriteQuotes)}
+          </div>
+        </section>
         {loading ? (
          <Loader />
          ) : (
@@ -46,7 +70,8 @@ function App() {
           filteredQuotes={filteredQuotes}
            categories={categories}
             category={category} 
-            handleCategoryChange={handleCategoryChange} 
+            handleCategoryChange={handleCategoryChange}
+            addToFavorites={addToFavorites} 
             />
          )}
         </main>
